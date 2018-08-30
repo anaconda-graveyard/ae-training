967237c29ead0dbe4e3848a767d27d728990d9b0
Kristopher Overholt <koverholt@gmail.com>
2018-08-30 17:28:04 -0500

    Updates

--- 1_fashion_mnist/download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p data
-curl -o data/train-images-idx3-ubyte.gz https://s3.amazonaws.com/anaconda-public-datasets/fashion-mnist/train-images-idx3-ubyte.gz
-curl -o data/train-labels-idx1-ubyte.gz https://s3.amazonaws.com/anaconda-public-datasets/fashion-mnist/train-labels-idx1-ubyte.gz
-curl -o data/t10k-images-idx3-ubyte.gz https://s3.amazonaws.com/anaconda-public-datasets/fashion-mnist/t10k-images-idx3-ubyte.gz
-curl -o data/t10k-labels-idx1-ubyte.gz https://s3.amazonaws.com/anaconda-public-datasets/fashion-mnist/t10k-labels-idx1-ubyte.gz
