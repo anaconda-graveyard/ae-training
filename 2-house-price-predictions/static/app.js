c1307886299965da87d7da704e9efcd8b763539d
Kristopher Overholt <koverholt@gmail.com>
2018-09-02 23:17:27 -0500

    Update house-price-predictions example

--- 2-house-price-predictions/static/app.js
+++ 2-house-price-predictions/static/app.js
@@ -1,7 +1,7 @@
 (function(angular) {
     'use strict';
   var myApp = angular.module('MyApp', []);
-  
+
   myApp.controller('MyCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.house = {};
         $scope.house.OverallQual = 7;
@@ -16,7 +16,7 @@
         $scope.house.GarageCars = 2;
         $scope.house.Fireplaces = 0;
         $scope.price = 0;
-        
+
         $scope.search = function () {
            $http.get('/api/predict', { params: $scope.house }).then(function(response) {
                 console.log(response.data);
@@ -25,7 +25,20 @@
                 console.log("failed :(", failure);
             });
         }
+  $(document).ready(function(){
+
+      $(window).scroll(function(){
+
+          if($(document).scrollTop() > 0) {
+              var newPos = $(document).scrollTop() + 0 ;
+              $('.mPrice').css( {top:newPos});
+          }
+
+          else {
+              $('.mPrice').css( {top:0});
+          }
+      })
+  })
   }]);
 
 })(window.angular);
-  
